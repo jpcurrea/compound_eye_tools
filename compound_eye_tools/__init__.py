9b979a26ac40e873160632f75ecb528da3cd31ce
jpcurrea <johnpaulcurrea@gmail.com>
2019-06-19 17:06:55 -0400

    change method for centering points in polar coordinates to avoid distortion near the boundaries. not tested yet.

--- compound_eye_tools/__init__.py
+++ compound_eye_tools/__init__.py
@@ -15,10 +15,16 @@ from numpy import linalg as LA
 def load_image(fn):
     return np.asarray(PIL.Image.open(fn))
 
-def print_progress(part, whole):
+def print_progress(part, whole, bar=True):
     prop = float(part)/float(whole)
     sys.stdout.write('\r')
-    sys.stdout.write("[%-20s] %d%%" % ("="*int(20*prop), 100*prop))
+    # sys.stdout.write("[%-20s] %d%%" % ("="*int(20*prop), round(100)*prop))
+    if bar:
+        prog_bar = '='*int(20*prop)
+        st = f"[{prog_bar:20s}] {round(100 * prop)}%"
+    else:
+        st = f"{round(100*prop)}%"
+    sys.stdout.write(st)
     sys.stdout.flush()
 
 def fit_line(data, component=0):             # fit 3d line to 3d data
@@ -50,7 +56,11 @@ def rotate(arr, theta, axis=0):
             [[np.cos(theta), -np.sin(theta), 0],
              [np.sin(theta), np.cos(theta), 0],
              [0, 0, 1]])
-    return np.dot(arr, rot_matrix)
+    nx, ny, nz = np.dot(arr, rot_matrix).T
+    nx = np.squeeze(nx)
+    ny = np.squeeze(ny)
+    nz = np.squeeze(nz)
+    return np.array([nx, ny, nz])
 
 def sphereFit(spX, spY, spZ):
     #   Assemble the f matrix
@@ -68,6 +78,9 @@ def sphereFit(spX, spY, spZ):
     return radius, np.squeeze(C[:-1])
 
 def cartesian_to_spherical(pts, center=np.array([0, 0, 0])):
+    # theta must be between 
+    # TODO: center points based on center of mass
+    # TODO: fix boundary problems for trig functions
     pts = pts - center
     radii = LA.norm(np.copy(pts), axis=1)
     theta = np.arccos(np.copy(pts)[:, 2]/radii)
@@ -102,9 +115,16 @@ class Points():
                  rotate_com=False):
         self.pts = np.array(arr)
         if arr.ndim > 1:
-            assert self.pts.shape[1] == 3, "Input array should have shape N x 3. Instead it has shape {} x {}.".format(self.pts.shape[0], self.pts.shape[1])
+            assert self.pts.shape[1] == 3, ("Input array should have shape "
+                                            "N x 3. Instead it has "
+                                            "shape {} x {}.".format(
+                                                self.pts.shape[0],
+                                                self.pts.shape[1])
         else:
-            assert self.pts.shape[0] == 3, "Input array should have shape 3 or N x 3. Instead it has shape {}.".format(self.pts.shape)
+            assert self.pts.shape[0] == 3, ("Input array should have shape "
+                                            "3 or N x 3. Instead it has "
+                                            "shape {}.".format(
+                                                self.pts.shape))
             self.pts = self.pts.reshape((1, -1))
         self.shape = self.pts.shape
         self.center = np.array([0, 0, 0])
@@ -116,23 +136,24 @@ class Points():
             # fit sphere
             x, y, z = self.pts.T
             self.radius, self.center = sphereFit(x, y, z)
-            # center points using the center of that sphere
             if center_points:
-                self.pts = self.pts -  self.center
+                # center points using the center of that sphere
+                self.pts = self.pts - self.center
                 self.center = self.center - self.center
-            self.spherical()
-            # rotate points using the center of mass in theta and phi
             if rotate_com:
+                # rotate points using the center of mass
+                # 1. find center of mass
                 com = self.pts.mean(0)
-                polar = cartesian_to_spherical(com.reshape((1, -1)))
-                self.polar_com = polar
-                theta_offset, phi_offset, r_m = np.squeeze(polar)
-                theta_offset = np.pi/2. - theta_offset
-                phi_offset = np.pi - phi_offset
-                self.pts = rotate(self.pts, theta_offset, axis=1)
-                self.pts = rotate(self.pts, theta_offset, axis=2)
-                self.pts = rotate(self.pts, phi_offset, axis=0)
-        # grab spherical coordinates of centered points
+                # 2. rotate com along x axis (com[0]) until z (com[2]) = 0
+                ang1 = np.arctan2(com[2], com[1])
+                com1 = rotate(com, ang1, axis=0)
+                rot1 = rotate(self.pts.T, ang1, axis=0)
+                # 3. rotate com along z axis (com[2]) until y (com[1]) = 0
+                ang2 = np.arctan2(com1[1], com1[0])
+                rot2 = rotate(rot1.T, ang2, axis=2)
+                self.pts = rot2
+            # grab spherical coordinates of centered points
+            self.spherical()
         self.x, self.y, self.z = self.pts.T
 
     def __len__(self):
